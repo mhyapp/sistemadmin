1edc2ebf9839676b6287f7d69f5f23a7b14e3d57
nvrasen <prasetiyantara@gmail.com>
2020-07-08 23:44:48 +0700

    read, insert nasabah

---
+++ application/models/Mod_nasabah.php
@@ -0,0 +1,33 @@
+<?php
+	
+	defined('BASEPATH') OR exit('No direct script access allowed');
+
+	/**
+	 * 
+	 */
+	class Mod_nasabah extends CI_Model
+	{
+        function get_nasabah()
+        {
+            return $this->db->get('nasabah')->result();
+        }
+
+        function tambahNasabah($datanasabah)
+        {
+            return $this->db->insert('nasabah', $datanasabah);
+        }
+
+        function get_nasabah_id($idnasabah)
+        {
+            return $this->db->get_where('nasabah',array('idnasabah'=>$idnasabah))->row_array();
+        }
+
+        function update_nasabah($idnasabah,$datanasabah)
+        {
+            $idnasabah = $this->input->post('idnasabah');
+            $this->db->where('idnasabah',$idnasabah);
+            return $this->db->update('nasabah',$datanasabah);
+        }
+    }
+    
+?>
