3f9233975989c7bcd5af29f73f6e254c2bb70128
nvrasen <prasetiyantara@gmail.com>
2020-07-16 23:27:39 +0700

    update report menu

--- application/views/dashboard_admin/sidebar.php
+++ application/views/dashboard_admin/sidebar.php
@@ -14,31 +14,43 @@
                     <li data-username="form elements advance componant validation masking wizard picker select" class="nav-item">
                         <a href="form_elements.html" class="nav-link "><span class="pcoded-micon"><i class="feather icon-file-text"></i></span><span class="pcoded-mtext">Jaminan</span></a>
                     </li>
-                    <li data-username="basic components Button Alert Badges breadcrumb Paggination progress Tooltip popovers Carousel Cards Collapse Tabs pills Modal Grid System Typography Extra Shadows Embeds" class="nav-item pcoded-hasmenu">
-                        <a href="javascript:" class="nav-link "><span class="pcoded-micon"><i class="feather icon-box"></i></span><span class="pcoded-mtext">Components</span></a>
-                        <ul class="pcoded-submenu">
-                            <li class=""><a href="bc_button.html" class="">Button</a></li>
-                            <li class=""><a href="bc_badges.html" class="">Badges</a></li>
-                            <li class=""><a href="bc_breadcrumb-pagination.html" class="">Breadcrumb & paggination</a></li>
-                            <li class=""><a href="bc_collapse.html" class="">Collapse</a></li>
-                            <li class=""><a href="bc_tabs.html" class="">Tabs & pills</a></li>
-                            <li class=""><a href="bc_typography.html" class="">Typography</a></li>
-
-
-                            <li class=""><a href="icon-feather.html" class="">Feather<span class="pcoded-badge label label-danger">NEW</span></a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item pcoded-menu-caption">
-                        <label>Settings</label>
-                    </li>
-                    <li data-username="Authentication Sign up Sign in reset password Change password Personal information profile settings map form subscribe" class="nav-item pcoded-hasmenu">
-                        <a href="javascript:" class="nav-link "><span class="pcoded-micon"><i class="feather icon-lock"></i></span><span class="pcoded-mtext">User</span></a>
-                        <ul class="pcoded-submenu">
-                            <li class=""><a href="<?php echo base_url('admin/user') ?>">Akun</a></li>
-                            <li class=""><a href="<?php echo base_url('admin/level') ?>">Level</a></li>
-                        </ul>
-                    </li>
                     <li data-username="form elements advance componant validation masking wizard picker select" class="nav-item">
                         <a href="form_elements.html" class="nav-link "><span class="pcoded-micon"><i class="feather icon-file-text"></i></span><span class="pcoded-mtext">Notaris</span></a>
                     </li>
+                    <li class="nav-item pcoded-menu-caption">
+                        <label>Transaction</label>
+                    </li>
+                    <li data-username="form elements advance componant validation masking wizard picker select" class="nav-item">
+                        <a href="<?php echo base_url('admin/tkeluar') ?>" class="nav-link "><span class="pcoded-micon"><i class="feather icon-file-text"></i></span><span class="pcoded-mtext">Transaksi Keluar</span></a>
+                    </li>
+                    <li data-username="form elements advance componant validation masking wizard picker select" class="nav-item">
+                        <a href="form_elements.html" class="nav-link "><span class="pcoded-micon"><i class="feather icon-file-text"></i></span><span class="pcoded-mtext">Transaksi Masuk</span></a>
+                    </li>
+                    <li class="nav-item pcoded-menu-caption">
+                        <label>Report</label>
+                    </li>
+                    <li data-username="form elements advance componant validation masking wizard picker select" class="nav-item">
+                        <a href="<?php echo base_url('admin/tkeluar') ?>" class="nav-link "><span class="pcoded-micon"><i class="feather icon-file-text"></i></span><span class="pcoded-mtext">Transaksi</span></a>
+                    </li>
+                    <li data-username="form elements advance componant validation masking wizard picker select" class="nav-item">
+                        <a href="form_elements.html" class="nav-link "><span class="pcoded-micon"><i class="feather icon-file-text"></i></span><span class="pcoded-mtext">Jaminan</span></a>
+                    </li>
+                    <?php
+                        if ($this->session->userdata('idleveluser') == "1"){
+                        
+                        ?>
+                        <li class="nav-item pcoded-menu-caption">
+                            <label>Settings</label>
+                        </li>
+                        <li data-username="Authentication Sign up Sign in reset password Change password Personal information profile settings map form subscribe" class="nav-item pcoded-hasmenu">
+                            <a href="javascript:" class="nav-link "><span class="pcoded-micon"><i class="feather icon-lock"></i></span><span class="pcoded-mtext">User</span></a>
+                            <ul class="pcoded-submenu">
+                                <li class=""><a href="<?php echo base_url('admin/user') ?>">Akun</a></li>
+                                <li class=""><a href="<?php echo base_url('admin/level') ?>">Level</a></li>
+                            </ul>
+                        </li>
+                        <li data-username="form elements advance componant validation masking wizard picker select" class="nav-item">
+                            <a href="form_elements.html" class="nav-link "><span class="pcoded-micon"><i class="feather icon-box"></i></span><span class="pcoded-mtext">Backup Database</span></a>
+                        </li>
+                    <?php } ?>
                 </ul>
